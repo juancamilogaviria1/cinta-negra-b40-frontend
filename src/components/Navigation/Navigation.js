3d0504b049b240801216e544d7465fce7acda958
juancamilogaviria1 <juancamilogaviria1@gmail.com>
2020-05-22 17:37:04 -0500

    configuracion de reactstrap y agregado de barra de navegación

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState} from 'react';
+import { Link } from 'react-router-dom';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+  } from 'reactstrap';
 
-const Navigation = () => {
-    return ( 
-
-        <h2>navbar</h2>
-     );
-}
- 
-export default Navigation;
+  const Navigation = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
+  
+    const toggle = () => setIsOpen(!isOpen);
+  
+    return (
+        <Navbar 
+            className="navbar navbar-dark bg-primary" 
+            color="dark" 
+            light expand="md">
+          <NavbarBrand tag={Link} to="/">Bneural</NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+              <NavLink tag={Link} to="/Login">Login</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} to="/signup">Signup</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+    );
+  }
+  
+  export default Navigation;
