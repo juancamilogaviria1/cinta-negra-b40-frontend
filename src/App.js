e476bbf218240721d3d5dcf7d603e680e394a352
juancamilogaviria1 <juancamilogaviria1@gmail.com>
2020-05-22 16:35:12 -0500

    Organizar componenetes y vistas de acuerdo a rutas en app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Login from './views/Login';
+import Home from './views/Home';
+
+//Componentes
+import Navigation from './components/Navigation';
+
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Navigation />
+      <Router>
+      <Switch>
+          <Route path="/Login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+   </Router>
+
+    </React.Fragment>
+    
+   
   );
 }
 
