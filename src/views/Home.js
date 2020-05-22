8df0c05d4a8533c3674d09117106273fd16d1706
juancamilogaviria1 <juancamilogaviria1@gmail.com>
2020-05-22 16:32:51 -0500

    Agregar vistas de Home y login

---
+++ src/views/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return ( 
+        <React.Fragment>
+            <h1>Hola desde Home</h1>
+        </React.Fragment>
+        
+     );
+}
+ 
+export default Home;
---
+++ src/views/Login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Login = () => {
+    return ( 
+        <h1>Hola desde Login Malditas perras</h1>
+     );
+}
+ 
+export default Login;
