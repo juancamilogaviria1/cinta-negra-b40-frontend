036dbc01ed43e0241cae34b15e93d47c69696f61
juancamilogaviria1 <juancamilogaviria1@gmail.com>
2020-05-23 02:47:18 -0500

    Agregar footer

---
+++ src/components/Navigation/Footer/Footer.js
@@ -0,0 +1,13 @@
+  
+import React from 'react';
+import './index.css';
+
+const Footer = () => {
+  return ( 
+    <footer className="footer text-white bg-dark">
+        © 2020 Copyright Camilo Gaviria
+    </footer>
+  );
+}
+ 
+export default Footer;
