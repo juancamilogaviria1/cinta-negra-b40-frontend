9609585a9d2e07dc56a6520a8e437f811295925c
juancamilogaviria1 <juancamilogaviria1@gmail.com>
2020-05-23 00:46:14 -0500

    Creacion de formulario Login

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import {
+     Button,
+      Form, 
+      FormGroup, 
+      Label, 
+      Input,
+     } from 'reactstrap';
 
-const Login = () => {
-    return ( 
-        <h1>Hola desde Login Malditas perras</h1>
-     );
+     const Login = () => {
+        return (
+          <React.Fragment>  
+                <h1>Login</h1>
+            <Form>
+                    <FormGroup>
+                    <Label>Email</Label>
+                    <Input type="email" name="email" id="exampleEmail" placeholder="type you email" />
+                    </FormGroup>
+                    <FormGroup>
+                    <Label for="examplePassword">Password</Label>
+                    <Input type="password" name="password" id="examplePassword" placeholder="type your password here" />
+                    </FormGroup>
+                    <Button>Submit</Button>
+            </Form>
+         </React.Fragment> 
+  );
 }
  
 export default Login;
